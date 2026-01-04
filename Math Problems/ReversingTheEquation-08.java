b9e919c1b1f83bec11384c1e4b8774e594216e9f
Manavshah07 <shahmanav197@gmail.com>
2026-01-04 19:11:24 +0530

    initial commit

--- Math Problems/ReversingTheEquation-08.java
+++ Math Problems/ReversingTheEquation-08.java
@@ -1,10 +1,41 @@
 // User function Template for Java
 // Question Link:-
 // https://www.geeksforgeeks.org/problems/reversing-the-equation2205/1
-// class Solution {
-// String reverseEqn(String S) {
-// // your code here
-// // int n = S.length;
 
-// }
-// }
+// User function Template for Java
+class Solution {
+    String reverseEqn(String S) {
+        // your code here
+
+        // WE WILL PUT OUR REVERSE EQUATION HERE IN ANS
+        StringBuilder ans = new StringBuilder();
+        int i = S.length() - 1;
+
+        while (i >= 0) {
+            // IF IT IS OPERATOR THEN WE ARE DIRECTLY ADDING TO THE ANS
+            if (S.charAt(i) == '+' || S.charAt(i) == '-'
+                    || S.charAt(i) == '*' || S.charAt(i) == '/') {
+                ans.append(S.charAt(i));
+            }
+
+            // HERE WE ARE COLLECTING ALL NUMBERS TOGETHER IN TEMP
+            StringBuilder temp = new StringBuilder();
+
+            // IF NUMBERS ARE FOUND THEN ADD IN TEMP AND SEARCH FOR NEXT NUMBER
+            while (i >= 0 && S.charAt(i) != '+' && S.charAt(i) != '-' && S.charAt(i) != '*' && S.charAt(i) != '/') {
+                temp.append(S.charAt(i));
+                i--;
+            }
+
+            // HERE WE ARE DOING I++ SO THAT MY NUMBER OR OPERATOR DON'T GET MISSED
+            if (temp.length() > 0)
+                i++;
+
+            // WE COLLECTED DIGIT BACKWARD SO WE REVERSE THEM LIKE TEMP = "02" → REVERSE →
+            // "20"
+            ans.append(temp.reverse());
+            i--;
+        }
+        return ans.toString();
+    }
+}
--- Math Problems/SumOfDigitIsPalindrome-10.java
+++ Math Problems/SumOfDigitIsPalindrome-10.java
@@ -1,24 +1,25 @@
 // User function Template for Java
+// Question Link:- https://practice.geeksforgeeks.org/problems/sum-of-digit-is-pallindrome-or-not2751/1  
 
 class Solution {
     boolean isDigitSumPalindrome(int n) {
         // code here
         int sum = 0;
-        while(n != 0) {
+        while (n != 0) {
             int rem = n % 10;
             sum += rem;
             n = n / 10;
         }
-        
+
         int original = sum;
         int reversed = 0;
-        while(sum != 0) {
+        while (sum != 0) {
             int rem = sum % 10;
-            reversed = reversed * 10 + rem; 
+            reversed = reversed * 10 + rem;
             sum = sum / 10;
         }
-        
-        if(original == reversed)
+
+        if (original == reversed)
             return true;
         else
             return false;
