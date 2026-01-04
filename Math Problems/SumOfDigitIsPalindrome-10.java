e0b9f5f7081d154d8a62998b0902d7d70ed1ad03
Manavshah07 <shahmanav197@gmail.com>
2026-01-04 18:04:03 +0530

    initial commit

---
+++ Math Problems/SumOfDigitIsPalindrome-10.java
@@ -0,0 +1,26 @@
+// User function Template for Java
+
+class Solution {
+    boolean isDigitSumPalindrome(int n) {
+        // code here
+        int sum = 0;
+        while(n != 0) {
+            int rem = n % 10;
+            sum += rem;
+            n = n / 10;
+        }
+        
+        int original = sum;
+        int reversed = 0;
+        while(sum != 0) {
+            int rem = sum % 10;
+            reversed = reversed * 10 + rem; 
+            sum = sum / 10;
+        }
+        
+        if(original == reversed)
+            return true;
+        else
+            return false;
+    }
+}
