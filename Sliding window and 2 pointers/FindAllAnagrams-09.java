13e82f8fab7b8da8a8ac716945edc8164d567712
Manavshah07 <shahmanav197@gmail.com>
2026-01-14 18:51:38 +0530

    initial commit

--- Sliding window and 2 pointers/FindAllAnagrams-09.java
+++ Sliding window and 2 pointers/FindAllAnagrams-09.java
@@ -1,6 +1,47 @@
 // Question Link:- https://leetcode.com/problems/find-all-anagrams-in-a-string/
 
 // BRUTE FORCE APPROACH WITH
+/**
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        // HERE WE CREATE A LIST ANS TO STORE OUR FINAL INDEXES
+        List<Integer> ans = new ArrayList<>();
+        int n = s.length();
+        int k = p.length(); 
+
+        // HERE WE CREATE A FREQUENCY ARRAY OF SIZE 26 TO STORE CHRACTER FROM A TO Z FROM STRING P
+        int[] freqP = new int[26];
+        for(char ch:p.toCharArray()) {
+            
+            // freqP[ch - 'a']++ means:
+            // 'a' - 'a' = 0 → index 0
+            // 'b' - 'a' = 1 → index 1
+            // 'c' - 'a' = 2 → index 2
+            // HERE WE ARE COUNTING THAT HOW MANY TIMES EACH CHARACTER COMES IN P
+            
+            freqP[ch - 'a']++;
+        }
+
+        // HERE WE EXTRACT SUBSTRINGS OF LENGTH K  AND IT WILL STORE LETTERS FROM START INTO THE STRING sub
+        // WE GO TILL N-K SO WE DON'T GO OUT OF BOUNDS
+        for(int i=0; i<=n-k; i++) {
+            String sub = s.substring(i, i+k);
+
+            // HERE WE CREATE A FREQUENCY ARRAY OF SIZE 26 TO STORE CHRACTER FROM A TO Z FROM STRING sub WHICH  
+            int[] freqS = new int[26];
+            for(char ch: sub.toCharArray()) {
+                freqS[ch - 'a']++;
+            }
+
+            // check if both arrays are identical 
+            if (Arrays.equals(freqP, freqS)) { 
+                ans.add(i); 
+            }
+        }
+        return ans;
+    }
+}
+*/
 
 // OPTIMAL APPROACH WITH TIME COMPLEXITY AS O(N) AND SPACE COMPLEXITY AS O(1) because map holds at most 26 letters
 import java.util.*;
