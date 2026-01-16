77509ee9f770a8369eba449780bf8bef08c279b1
Manavshah07 <shahmanav197@gmail.com>
2026-01-16 17:37:27 +0530

    initial commit

--- Sliding window and 2 pointers/LongestRepeatingCharacterReplacement-12.java
+++ Sliding window and 2 pointers/LongestRepeatingCharacterReplacement-12.java
@@ -77,3 +77,39 @@ class Solution {
 // return maxLength;
 // }
 // }
+
+// MORE OPTIMAL APPROACH WITH TIME COMPLEXITY AS O(N) INSIDE WHILE WE ARE
+// CHECKING EXTRA CHARACTER OR HASH OF FREQUENCY AND SPACE COMPLEXITY AS O(26)
+// class Solution {
+// public int characterReplacement(String s, int k) {
+// int i = 0, j = 0, maxLength = 0, maxFreq = 0;
+// int n = s.length();
+// HashMap<Character, Integer> hmap = new HashMap<>();
+
+// while(j < n) {
+// char ch = s.charAt(j);
+// hmap.put(ch, hmap.getOrDefault(ch, 0) + 1);
+
+// // HERE WE WILL GET THE MAXFREQ OF THE CHARACTER
+// maxFreq = Math.max(maxFreq, hmap.get(ch));
+
+// // HERE IF NEED TO CHANGE THE CHARACTER MORE THAN K THEN WE WILL START
+// REMOVING THE CHARACTER FROM START I.E I
+// // HERE IT WILL SHRINK ONLY ONCE
+// if((j-i+1) - maxFreq > k) {
+// char ch1 = s.charAt(i);
+// hmap.put(ch1, hmap.get(ch1) - 1);
+
+// i++;
+// }
+
+// if((j-i+1) - maxFreq <= k)
+// maxLength = Math.max(maxLength, j-i+1);
+// else
+// break;
+// j++;
+// }
+
+// return maxLength;
+// }
+// }
