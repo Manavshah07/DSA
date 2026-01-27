45934407ec4c0221fb1f952ca02a46760842579e
Manavshah07 <shahmanav197@gmail.com>
2026-01-27 17:12:12 +0530

    initial commit

--- Sliding window and 2 pointers/SubArraysWithKDifferentIntegers-16.java
+++ Sliding window and 2 pointers/SubArraysWithKDifferentIntegers-16.java
@@ -1,4 +1,24 @@
 // Question Link:-
 // https://leetcode.com/problems/subarrays-with-k-different-integers/description/
 
-// BRUTE FORCE APPROACH WITH TIME COMPLEXITY AS O(N^2) AND SPACE COMPLEXITY
+// BRUTE FORCE APPROACH WITH TIME COMPLEXITY AS O(N^2) AND SPACE COMPLEXITY WITH O(N)
+import java.util.*;
+
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        int n = nums.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            HashMap<Integer, Integer> hmap = new HashMap<>();
+            for (int j = i; j < n; j++) {
+                hmap.put(nums[j], hmap.getOrDefault(nums[j], 0) + 1);
+
+                if (hmap.size() == k)
+                    count++;
+                else if (hmap.size() > k)
+                    break;
+            }
+        }
+        return count;
+    }
+}
