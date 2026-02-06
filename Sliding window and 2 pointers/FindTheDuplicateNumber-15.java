f3cc8b56a273561fbed9129737ad2c8a7beabc3e
Manavshah07 <shahmanav197@gmail.com>
2026-02-06 11:07:27 +0530

    initial commit

--- Sliding window and 2 pointers/FindTheDuplicateNumber-15.java
+++ Sliding window and 2 pointers/FindTheDuplicateNumber-15.java
@@ -1,5 +1,7 @@
 // Question Link:- https://leetcode.com/problems/find-the-duplicate-number/submissions/1892568987/
 
+import java.util.*;
+
 // BRUTE FORCE APPROACH WITH TIME COMPLEXITY AS O(N^2) AND SPACE COMPLEXITY AS O(1)
 class Solution {
     public int findDuplicate(int[] nums) {
@@ -17,17 +19,17 @@ class Solution {
 
 // OPTIMAL APPROACH WITH TIME COMPLEXITY AS O(N) AND SPACE COMPLEXITY AS O(N)
 // BECAUSE OF HASHMAP AS EXTRA SPACE IS USED
-// class Solution {
-// public int findDuplicate(int[] nums) {
-// int n = nums.length;
-// HashMap<Integer, Integer> hmap = new HashMap<>();
+class Solution2 {
+    public int findDuplicate(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> hmap = new HashMap<>();
 
-// for(int i=0; i<n; i++) {
-// hmap.put(nums[i], hmap.getOrDefault(nums[i], 0) + 1);
+        for (int i = 0; i < n; i++) {
+            hmap.put(nums[i], hmap.getOrDefault(nums[i], 0) + 1);
 
-// if(hmap.get(nums[i]) > 1)
-// return nums[i];
-// }
-// return -1;
-// }
-// }
+            if (hmap.get(nums[i]) > 1)
+                return nums[i];
+        }
+        return -1;
+    }
+}
