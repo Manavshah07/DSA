732f8a00e0a777d88de175b2de69ad31a1e9496f
Manavshah07 <shahmanav197@gmail.com>
2026-02-08 21:05:34 +0530

    initial commit

--- Sliding window and 2 pointers/LongestSubstringWithoutRepeatingCharacters-03.java
+++ Sliding window and 2 pointers/LongestSubstringWithoutRepeatingCharacters-03.java
@@ -35,35 +35,35 @@ class Solution {
 // OPTIMAL APPROACH : TIME COMPLEXITY:-O(N) AND SPACE COMPLEXITY:- O(N) BECAUSE
 // USING HASHMAP
 
-// class Solution {
-// public int lengthOfLongestSubstring(String s) {
-// int n = s.length();
-// int ans = 0;
-// int i=0;
-// int j=0;
-// HashMap<Character, Integer> hmap = new HashMap<>();
+class Solution2 {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        int i = 0;
+        int j = 0;
+        HashMap<Character, Integer> hmap = new HashMap<>();
 
-// while(j < n) {
-// char ch = s.charAt(j);
-// hmap.put(ch, hmap.getOrDefault(ch, 0) + 1);
+        while (j < n) {
+            char ch = s.charAt(j);
+            hmap.put(ch, hmap.getOrDefault(ch, 0) + 1);
 
-// if(hmap.size() == j-i+1) {
-// // HERE WE GOT THE UNIQUE CHARACTERS
-// ans = Math.max(ans, j-i+1);
-// j++;
-// } else if(hmap.size() < j-i+1) {
-// // HERE DUPLICATE CHARACTER OCCURS IN MY MAP
-// while(hmap.size() < j-i+1) {
-// char ch1 = s.charAt(i);
-// hmap.put(ch1, hmap.get(ch1) - 1);
+            if (hmap.size() == j - i + 1) {
+                // HERE WE GOT THE UNIQUE CHARACTERS
+                ans = Math.max(ans, j - i + 1);
+                j++;
+            } else if (hmap.size() < j - i + 1) {
+                // HERE DUPLICATE CHARACTER OCCURS IN MY MAP
+                while (hmap.size() < j - i + 1) {
+                    char ch1 = s.charAt(i);
+                    hmap.put(ch1, hmap.get(ch1) - 1);
 
-// if(hmap.get(ch1) == 0)
-// hmap.remove(ch1);
-// i++;
-// }
-// j++;
-// }
-// }
-// return ans;
-// }
-// }
+                    if (hmap.get(ch1) == 0)
+                        hmap.remove(ch1);
+                    i++;
+                }
+                j++;
+            }
+        }
+        return ans;
+    }
+}
