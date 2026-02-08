c5d58914fe6b8a56f7dc26ed3b674cd417eb9fee
Manavshah07 <shahmanav197@gmail.com>
2026-02-08 20:32:25 +0530

    initial commit

--- Sliding window and 2 pointers/LongestRepeatingCharacterReplacement-12.java
+++ Sliding window and 2 pointers/LongestRepeatingCharacterReplacement-12.java
@@ -1,5 +1,7 @@
 // Question Link:- https://leetcode.com/problems/longest-repeating-character-replacement/
 
+import java.util.*;
+
 // BRUTE FORCE WITH TIME COMPLEXITY AS O(N^2) AND SPACE COMPLEXITY AS O(26) AS WE ARE USING EXTERNAL SPACE OF STORING 26 LETTERS INTO THE ARRAY
 class Solution {
     public int characterReplacement(String s, int k) {
@@ -36,47 +38,47 @@ class Solution {
 
 // OPTIMAL APPROACH WITH TIME COMPLEXITY AS O(N + N) * 26 INSIDE WHILE WE ARE
 // CHECKING EXTRA CHARACTER OR HASH OF FREQUENCY AND SPACE COMPLEXITY AS O(26)
-// class Solution {
-// public int characterReplacement(String s, int k) {
-// int i = 0, j = 0, maxLength = 0, maxFreq = 0;
-// int n = s.length();
-// HashMap<Character, Integer> hmap = new HashMap<>();
+class Solution2 {
+    public int characterReplacement(String s, int k) {
+        int i = 0, j = 0, maxLength = 0, maxFreq = 0;
+        int n = s.length();
+        HashMap<Character, Integer> hmap = new HashMap<>();
 
-// while(j < n) {
-// char ch = s.charAt(j);
-// hmap.put(ch, hmap.getOrDefault(ch, 0) + 1);
+        while (j < n) {
+            char ch = s.charAt(j);
+            hmap.put(ch, hmap.getOrDefault(ch, 0) + 1);
 
-// // HERE WE WILL GET THE MAXFREQ OF THE CHARACTER
-// maxFreq = Math.max(maxFreq, hmap.get(ch));
+            // HERE WE WILL GET THE MAXFREQ OF THE CHARACTER
+            maxFreq = Math.max(maxFreq, hmap.get(ch));
 
-// // HERE IF NEED TO CHANGE THE CHARACTER MORE THAN K THEN WE WILL START
-// REMOVING THE CHARACTER FROM START I.E I
-// while((j-i+1) - maxFreq > k) {
-// char ch1 = s.charAt(i);
-// hmap.put(ch1, hmap.get(ch1) - 1);
+            // HERE IF NEED TO CHANGE THE CHARACTER MORE THAN K THEN WE WILL START REMOVING
+            // THE CHARACTER FROM START I.E I
+            while ((j - i + 1) - maxFreq > k) {
+                char ch1 = s.charAt(i);
+                hmap.put(ch1, hmap.get(ch1) - 1);
 
-// // HERE WE SHRINK OUR WINDOW BY REMOVING FIRST CHARACTER OF I OR FROM LEFT
-// SIDE AND AFTER THAT DOING I++ SO FOR THAT WE WILL RECALCUATE OUR MAXFREQ
-// // recalculate maxFreq
-// maxFreq = 0;
+                // HERE WE SHRINK OUR WINDOW BY REMOVING FIRST CHARACTER OF I OR FROM LEFT SIDE
+                // AND AFTER THAT DOING I++ SO FOR THAT WE WILL RECALCUATE OUR MAXFREQ
+                // recalculate maxFreq
+                maxFreq = 0;
 
-// // HERE THIS LOOP IS ALSO USED FOR RECALCULATING THE MAXFREQ
-// for (int val : hmap.values()) {
-// maxFreq = Math.max(maxFreq, val);
-// }
-// i++;
-// }
+                // HERE THIS LOOP IS ALSO USED FOR RECALCULATING THE MAXFREQ
+                for (int val : hmap.values()) {
+                    maxFreq = Math.max(maxFreq, val);
+                }
+                i++;
+            }
 
-// if((j-i+1) - maxFreq <= k)
-// maxLength = Math.max(maxLength, j-i+1);
-// else
-// break;
-// j++;
-// }
+            if ((j - i + 1) - maxFreq <= k)
+                maxLength = Math.max(maxLength, j - i + 1);
+            else
+                break;
+            j++;
+        }
 
-// return maxLength;
-// }
-// }
+        return maxLength;
+    }
+}
 
 // MORE OPTIMAL APPROACH WITH TIME COMPLEXITY AS O(N) INSIDE WHILE WE ARE
 // CHECKING EXTRA CHARACTER OR HASH OF FREQUENCY AND SPACE COMPLEXITY AS O(26)
