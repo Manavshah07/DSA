047234d7d53de5905808b3ae59c8e592c1112022
Manavshah07 <shahmanav197@gmail.com>
2026-02-09 20:03:33 +0530

    initial commit

--- Sliding window and 2 pointers/MinimumWndowSubstrings-17.java
+++ Sliding window and 2 pointers/MinimumWndowSubstrings-17.java
@@ -1,4 +1,6 @@
+
 // Question Link:- https://leetcode.com/problems/minimum-window-substring/
+import java.util.*;
 
 // BRUTE FORCE APPROACH WITH TIME COMPLEXITY AS O(N^2) AND SPACE COMPLEXITY AS O(256)
 class Solution {
@@ -59,65 +61,65 @@ class Solution {
 
 // OPTIMISED APPROACH WITH TIME COMPLEXITY AS O(2N) + O(M) AND SPACE COMPLEXITY
 // AS O(256)
-// class Solution {
-// public String minWindow(String s, String t) {
-// int n = s.length();
-// int m = t.length();
-// int minLength = Integer.MAX_VALUE;
-// int startIndex = -1;
-// int i = 0, j = 0, count = 0;
-// HashMap<Character, Integer> hmap = new HashMap<>();
+class Solution2 {
+    public String minWindow(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int minLength = Integer.MAX_VALUE;
+        int startIndex = -1;
+        int i = 0, j = 0, count = 0;
 
-// // ADDING THE CHARACTER OF STRING T INTO THE HASHMAP
-// // TIME COMPLEXITY FOR THIS LOOP IS O(M)
-// for (int k = 0; k < t.length(); k++) {
-// char ch = t.charAt(k);
-// hmap.put(ch, hmap.getOrDefault(ch, 0) + 1);
-// }
+        HashMap<Character, Integer> hmap = new HashMap<>();
 
-// // TIME COMPLEXITY FOR THIS LOOP IS O(n)
-// while (j < s.length()) {
-// char ch = s.charAt(j);
+        // ADDING THE CHARACTER OF STRING T INTO THE HASHMAP
+        // TIME COMPLEXITY FOR THIS LOOP IS O(M)
+        for (int k = 0; k < t.length(); k++) {
+            char ch = t.charAt(k);
+            hmap.put(ch, hmap.getOrDefault(ch, 0) + 1);
+        }
 
-// // IF HMAP HAS THE CHARACTER WHICH IS PRESENT IN STRING T AND AFTER THAT WE
-// ARE
-// // CHECKING THAT CHARACTER FREQUENCY IS GREATER THAN 0 THEN WE ARE INCREASING
-// // THE COUNT AND DECREMENTING ITS FREQUENCY FROM THE MAP
-// if (hmap.containsKey(ch)) {
-// if (hmap.get(ch) > 0)
-// count += 1;
-// hmap.put(ch, hmap.get(ch) - 1);
-// }
+        // TIME COMPLEXITY FOR THIS LOOP IS O(n)
+        while (j < s.length()) {
+            char ch = s.charAt(j);
 
-// // IF MY COUNT IS EQUAL TO STRING T LENGTH THEN WE WILL CALCULATE MINLENGTH
-// // TIME COMPLEXITY FOR THIS LOOP IS O(N) AND THIS WILL RUN IN MAXIMUM WORST
-// CASE
-// while (count == m) {
-// if (j - i + 1 < minLength) {
-// minLength = j - i + 1;
-// // WE USED STARTINDEX TO CALCUATE THE MINIMUM STRING
-// startIndex = i;
-// }
+// IF HMAP HAS THE CHARACTER WHICH IS PRESENT IN STRING T AND AFTER THAT WE ARE
+// CHECKING THAT CHARACTER FREQUENCY IS GREATER THAN 0 THEN WE ARE INCREASING
+// THE COUNT AND DECREMENTING ITS FREQUENCY FROM THE MAP
+if (hmap.containsKey(ch)) {
+if (hmap.get(ch) > 0)
+count += 1;
+hmap.put(ch, hmap.get(ch) - 1);
+}
 
-// // HERE WE ARE CALCULATING FOR ITH INDEX POSITION
-// char leftChar = s.charAt(i);
-// // IF MY CHARACTER CONTAINS IN HASHMAP THEN WE NEED DO ALL THE CALCULATIONS
-// THAT
-// // ARE THEIR PREVIOUSLY SO WE ARE JUST INCREASING THE FREQUENCY OF THAT
-// // CHARACTER AND AFTER THAT IF ITS FREQUENCT IS GREATER THAN 0 SO WE ARE
-// DOING
-// // COUNT-- AS WE SHRINKING OUR WINDOW SO WE WILL REMOVE THAT CHARACTER
-// if (hmap.containsKey(leftChar)) {
-// hmap.put(leftChar, hmap.get(leftChar) + 1);
-// if (hmap.get(leftChar) > 0)
-// count--;
-// }
-// i++;
-// }
-// j++;
-// }
-// if (startIndex == -1)
-// return "";
-// return s.substring(startIndex, startIndex + minLength);
-// }
-// }
+// IF MY COUNT IS EQUAL TO STRING T LENGTH THEN WE WILL CALCULATE MINLENGTH
+// TIME COMPLEXITY FOR THIS LOOP IS O(N) AND THIS WILL RUN IN MAXIMUM WORST
+CASE
+while (count == m) {
+if (j - i + 1 < minLength) {
+minLength = j - i + 1;
+// WE USED STARTINDEX TO CALCUATE THE MINIMUM STRING
+startIndex = i;
+}
+
+// HERE WE ARE CALCULATING FOR ITH INDEX POSITION
+char leftChar = s.charAt(i);
+// IF MY CHARACTER CONTAINS IN HASHMAP THEN WE NEED DO ALL THE CALCULATIONS
+THAT
+// ARE THEIR PREVIOUSLY SO WE ARE JUST INCREASING THE FREQUENCY OF THAT
+// CHARACTER AND AFTER THAT IF ITS FREQUENCT IS GREATER THAN 0 SO WE ARE
+DOING
+// COUNT-- AS WE SHRINKING OUR WINDOW SO WE WILL REMOVE THAT CHARACTER
+if (hmap.containsKey(leftChar)) {
+hmap.put(leftChar, hmap.get(leftChar) + 1);
+if (hmap.get(leftChar) > 0)
+count--;
+}
+i++;
+}
+j++;
+}
+if (startIndex == -1)
+return "";
+return s.substring(startIndex, startIndex + minLength);
+}
+}
