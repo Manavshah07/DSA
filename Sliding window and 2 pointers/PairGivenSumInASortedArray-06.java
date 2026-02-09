4328386f33e505623615198616e509b985b03a3b
Manavshah07 <shahmanav197@gmail.com>
2026-02-09 20:08:51 +0530

    initial commit

--- Sliding window and 2 pointers/PairGivenSumInASortedArray-06.java
+++ Sliding window and 2 pointers/PairGivenSumInASortedArray-06.java
@@ -1,6 +1,8 @@
 // User function Template for Java
 // Question Link:- https://www.geeksforgeeks.org/problems/pair-with-given-sum-in-a-sorted-array4940/1 
 
+import java.util.*;
+
 // BRUTE FORCE WITH TIME COMPLEXITY AS O(N^2) AND SPACE COMPLEXITY AS O(1)
 class Solution {
     int countPairs(int arr[], int target) {
@@ -16,24 +18,24 @@ class Solution {
     }
 }
 
+// OPTIMAL APPROACH USING HASHMAP AND TIME COMPLEXITY AS O(N) AND SPACE
+// COMPLEXITY AS O(N) USING HASHMAP
+class Solution2 {
+    int countPairs(int arr[], int target) {
+        // Complete the function
+        int count = 0;
+        HashMap<Integer, Integer> hmap = new HashMap<>();
 
+        for (int i = 0; i < arr.length; i++) {
+            int pair = target - arr[i];
 
-// OPTIMAL APPROACH USING HASHMAP AND TIME COMPLEXITY AS O(N) AND SPACE COMPLEXITY AS O(N) USING HASHMAP
-// class Solution {
-//     int countPairs(int arr[], int target) {
-//         // Complete the function
-//         int count = 0;
-//         HashMap<Integer, Integer> hmap = new HashMap<>();
-        
-//         for(int i=0; i<arr.length; i++) {
-//             int pair = target - arr[i];
-            
-//             // HERE AS A DUPLICATE NUMBER OCCURS WE FIRST INCREASE COUNT AND AFTER THAT WE STORE IN MAP
-//             if(hmap.containsKey(pair))
-//                 count+=hmap.get(pair);
-                
-//             hmap.put(arr[i], hmap.getOrDefault(arr[i], 0) + 1);
-//         }
-//         return count;
-//     }
-// }
+            // HERE AS A DUPLICATE NUMBER OCCURS WE FIRST INCREASE COUNT AND AFTER THAT WE
+            // STORE IN MAP
+            if (hmap.containsKey(pair))
+                count += hmap.get(pair);
+
+            hmap.put(arr[i], hmap.getOrDefault(arr[i], 0) + 1);
+        }
+        return count;
+    }
+}
