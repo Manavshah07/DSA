ae2de57d6915936fc196cc68e360ba24ab89812e
Manavshah07 <shahmanav197@gmail.com>
2026-02-11 18:04:03 +0530

    initial commit

--- Sliding window and 2 pointers/PairGivenSumInASortedArray-06.java
+++ Sliding window and 2 pointers/PairGivenSumInASortedArray-06.java
@@ -37,6 +37,6 @@ class Solution2 {
             hmap.put(arr[i], hmap.getOrDefault(arr[i], 0) + 1);
         }
         return count;
-        // jkjsadj
+        // jkjsadj hjhdkjashd gdskasndhg ggjsad
     }
 }
