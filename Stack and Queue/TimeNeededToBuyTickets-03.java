dd7b42dd290a9a7b45933a1f8ef11e36f5a64dd4
Manavshah07 <shahmanav197@gmail.com>
2026-02-13 19:45:09 +0530

    initial commit

--- Stack and Queue/TimeNeededToBuyTickets-03.java
+++ Stack and Queue/TimeNeededToBuyTickets-03.java
@@ -1,5 +1,4 @@
 // Question Link:-
 // https://leetcode.com/problems/time-needed-to-buy-tickets/description/
 
-// BRUTE FORCE APPROACH WITH TIME COMPLEXITY AS O(N^2) AND SPACE COMPLEXITY AS
-// O(1)
+// BRUTE FORCE APPROACH WITH TIME COMPLEXITY AS O(N^2) AND SPACE COMPLEXITY AS O(1)
