68ee2f5ce4e2a869414dcc196cb35d5841decb4a
Manavshah07 <shahmanav197@gmail.com>
2026-02-16 19:51:12 +0530

    initial commit

--- Stack and Queue/TimeNeededToBuyTickets-03.java
+++ Stack and Queue/TimeNeededToBuyTickets-03.java
@@ -5,3 +5,18 @@
 // O(1)
 
 // OPTIMAL APPROACH WITH TIME COMPLEXITY AS
+class Solution2 {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time = 0;
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+
+                time = time + Math.min(tickets[i], tickets[k]);
+            } else {
+                //
+                time = time + Math.min(tickets[i], tickets[k] - 1);
+            }
+        }
+        return time;
+    }
+}
