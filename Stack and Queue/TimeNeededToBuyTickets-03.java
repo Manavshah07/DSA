01c009c28218d7ce8bba2506bced02b1fae9e40d
Manavshah07 <shahmanav197@gmail.com>
2026-02-17 18:37:19 +0530

    initial commit

--- Stack and Queue/TimeNeededToBuyTickets-03.java
+++ Stack and Queue/TimeNeededToBuyTickets-03.java
@@ -1,7 +1,4 @@
-// Question Link:-
-// https://leetcode.com/problems/time-needed-to-buy-tickets/description/
-
-// BRUTE FORCE APPROACH WITH TIME COMPLEXITY AS O(N^2) AND SPACE COMPLEXITY AS O(1)
+// Question Link:- https://leetcode.com/problems/time-needed-to-buy-tickets/description/
 
 // APPROACH WITH TIME COMPLEXITY AS O(N * MAXTICKET(i)) WHICH IS O(N * M) AND SPACE COMPLEXITY IS O(N) AS WE ARE USING QUEUE
 import java.util.*;
@@ -40,18 +37,3 @@ class Solution {
 }
 
 // OPTIMAL APPROACH WITH TIME COMPLEXITY AS O(N) AND SPACE COMPLEXITY AS O(N)
-class Solution2 {
-    public int timeRequiredToBuy(int[] tickets, int k) {
-        int time = 0;
-        for (int i = 0; i < tickets.length; i++) {
-            if (i <= k) {
-
-                time = time + Math.min(tickets[i], tickets[k]);
-            } else {
-                //
-                time = time + Math.min(tickets[i], tickets[k] - 1);
-            }
-        }
-        return time;
-    }
-}
