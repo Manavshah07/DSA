31ab20a9302ff650a54e5eb4a2080e62b7f93d6d
Manavshah07 <shahmanav197@gmail.com>
2026-02-18 18:26:57 +0530

    initial commit

--- Stack and Queue/NextGreaterElements-04.java
+++ Stack and Queue/NextGreaterElements-04.java
@@ -1,2 +1,7 @@
 // Question Link:-
 // http://geeksforgeeks.org/problems/next-larger-element-1587115620/1
+
+/**
+ * MONOTONIC STACK = IT MEANS THAT WE ARE STORING THE ELEMENTS IN A SPECIFIC
+ * ORDER I.E. INCREASING ORDER OR DECREASING ORDER.
+ */
