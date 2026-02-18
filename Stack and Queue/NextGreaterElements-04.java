d04d3a6f57fc9f61214452a13f3abd845a555ec8
Manavshah07 <shahmanav197@gmail.com>
2026-02-18 19:36:30 +0530

    initial commit

--- Stack and Queue/NextGreaterElements-04.java
+++ Stack and Queue/NextGreaterElements-04.java
@@ -30,10 +30,42 @@ class Solution {
  */
 
 /**
- *              4  12  5  3  1  2  5  3  1  2  4   6
- * new array => 12 -1  6  5  2  5  6  4  2  4  6  -1
+ * 4 12 5 3 1 2 5 3 1 2 4 6
+ * new array => 12 -1 6 5 2 5 6 4 2 4 6 -1
  * 
  * (HERE WE ARE STORING ELEMENTS IN DECREASING ORDER IN THE MONOTONIC STACK)
  */
 
-// OPTIMISED APPROACH
+// OPTIMISED APPROACH WITH TIME COMPLEXITY AS O(2N) AND SPACE COMPLEXITY AS
+// O(2N) ONE N IS FOR ARRAYLIST AND ANOTHER IS FOR STACK
+
+class Solution2 {
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+        int n = arr.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            // IF MY STACK HAS SOME ELEMENTS AND THE TOP IS LESS THAN CURRENT ELEMENT I.E.
+            // ARR[i] THEN WE WILL REMOVE THE TOP ELEMENT
+
+            // IT CAN BE PERFORMED SOMETIME FOR 3 ELEMENT SOMETIME FOR 2 AND MAX IT CAN BE
+            // FOR N
+            while (!st.isEmpty() && st.peek() <= arr[i])
+                st.pop();
+
+            // IF MY STACK IS EMPTY THEN WE ADD -1
+            if (st.isEmpty())
+                // HERE WE ARE STORING AS 0 BECAUSE WE WANT ANS AS LEFT-TO-RIGHT AND IN ARRAY 0
+                // COMES AS START
+                ans.add(0, -1);
+            else
+                ans.add(0, st.peek());
+
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+}
